081d0dfd68a2c635c14a14b53a5afd9bde70d92b
Matheus Portilho <61274419+Helped64@users.noreply.github.com>
2020-06-22 21:57:35 -0300

    Commit Inicial de criação do Projeto

---
+++ src/main/java/br/mack/App.java
@@ -0,0 +1,13 @@
+package br.mack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
